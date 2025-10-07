fcc478cd2c0431bad3f86ea192c39b626570e4b0
phamducdat13 <169007980+phamducdat13112003@users.noreply.github.com>
2025-10-08 01:41:33 +0700

    init

---
+++ src/test/java/fpt/edu/vn/gms/GmsApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.edu.vn.gms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
