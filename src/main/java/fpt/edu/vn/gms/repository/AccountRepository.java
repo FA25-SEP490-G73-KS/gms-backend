f84c058a5b192686808eb6260ae74366500c0168
namtphe161280 <101730245+namtphe161280@users.noreply.github.com>
2025-10-20 19:51:15 +0700

    authentication
    login, logout, forgot password (SMS)

--- src/main/java/fpt/edu/vn/gms/repository/AccountRepository.java
+++ src/main/java/fpt/edu/vn/gms/repository/AccountRepository.java
@@ -2,12 +2,9 @@ package fpt.edu.vn.gms.repository;
 
 import fpt.edu.vn.gms.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByUsername(String username);
     Optional<Account> findByPhone(String phone);
-    boolean existsByPhone(String phone);
 }
